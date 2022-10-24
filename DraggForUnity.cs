2ca196bc6e0a810c4826c7c00667dfe63dc6bcb0
KoolakStudio <116540217+KoolakStudio@users.noreply.github.com>
2022-10-24 10:32:39 +0330

    first version
    this class use for unity object when you want drag and drop Event .

---
+++ DraggForUnity.cs
@@ -0,0 +1,39 @@
+﻿//using System.Collections;
+//using System.Collections.Generic;
+using UnityEngine;
+
+public class MyDragg : MonoBehaviour
+{
+    private float startPosX;
+    private float startPosY;
+    private bool isBeingHeld = true;
+    private void OnMouseDrag()
+    {
+        if (isBeingHeld)
+        {
+            //When Move Or Dragg Happen And You Held Key Or Touch
+            Vector3 mousePos;
+            mousePos = Input.mousePosition;
+            mousePos = Camera.main.ScreenToWorldPoint(mousePos);
+
+            this.gameObject.transform.localPosition = new Vector3(mousePos.x - startPosX, mousePos.y - startPosY, 0);
+        }
+    }
+    private void OnMouseDown()
+    {
+        //Start Touch Or Click On Object
+        Vector3 mousePos;
+        mousePos = Input.mousePosition;
+        mousePos = Camera.main.ScreenToWorldPoint(mousePos);
+        startPosX = mousePos.x - this.transform.localPosition.x;
+        startPosY = mousePos.y - this.transform.localPosition.y;
+        isBeingHeld = true;
+    }
+    private void OnMouseUp()
+    {
+        //Drop Event Happen
+        isBeingHeld = false;
+    }
+
+
+}
